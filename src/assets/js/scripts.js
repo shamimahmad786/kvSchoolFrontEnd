cd841009b0b2643d46716fba3052486f5c12f660
ashish00as <ashish11608111@gmail.com>
2024-02-02 18:45:16 +0530

    dashboard changes

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -71,8 +71,8 @@ var publcKey;
 function getKey() {
     $.ajax({
         //  url: "https://kvsonlinetransfer.kvs.gov.in/MOE-RAD-TEACHER/api/login/getKey",
-        //  url: "http://10.25.26.251:8014/api/login/getKey",
-       url: "https://demopgi.udiseplus.gov.in/MOE-RAD-TEACHER-V1/api/login/getKey",
+         url: "http://10.25.26.251:8014/api/login/getKey",
+    //    url: "https://demopgi.udiseplus.gov.in/MOE-RAD-TEACHER-V1/api/login/getKey",
         type: "POST",
         cache: false,
         async: false,
